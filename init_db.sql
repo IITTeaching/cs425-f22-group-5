e9b6ff76d28f44aed22c163f544d2e788d830b57
Charles Saineghi <charles@delltoo.localdomain>
2022-12-06 13:11:08 -0600

    added foreign keys, changed loan and account type defs

--- init_db.sql
+++ init_db.sql
@@ -16,49 +16,54 @@ CREATE TABLE branch (
 CREATE TABLE employee (
     ssn             INTEGER NOT NULL,
     name            VARCHAR(50),
+    passwd          VARCHAR(50),
     addr            VARCHAR(20),
     branch          INTEGER,         -- foreign key to branch_id in branch
     position        VARCHAR(20),
     salary          NUMERIC(10,2),
-    PRIMARY KEY     (ssn)
-    FOREIGN KEY     (branch) REFERENCES branch
+    PRIMARY KEY     (ssn),
+    FOREIGN KEY     (branch) REFERENCES branch (addr)
 );
 
 CREATE TABLE customer (
-    customer_id     INTEGER NOT NULL,
+    customer_id     SERIAL,
     name            VARCHAR(50),
+    passwd          VARCHAR(50),
     addr            VARCHAR(20),
-    branch          INTEGER         -- foreign key to branch_id in branch
+    branch          INTEGER,        -- foreign key to branch_id in branch
+    PRIMARY KEY     (customer_id),
+    FOREIGN KEY     (branch) REFERENCES branch
 );
+
+CREATE TYPE account_t AS ENUM ('SAVINGS', 'CHECKING');
 
 CREATE TABLE account (
+    account_owner   INTEGER,
     account_num     INTEGER NOT NULL,
     account_type    account_t,
-    balance         NUMERIC(10,2)
+    balance         NUMERIC(10,2),
+    FOREIGN KEY     (account_owner) REFERENCES customer
 );
-
-CREATE DOMAIN account_t AS
-    VARCHAR NOT NULL CHECK (value = "CHECKING" OR value = "SAVINGS"); -- or other accounts?
 
 CREATE TABLE account_type (
     account_type    account_t,
     fee             NUMERIC(4,2),
-    addr            VARCHAR(20)     -- foreign key to addr in branch
+    addr            VARCHAR(20),     -- foreign key to addr in branch
+    FOREIGN KEY     (addr) REFERENCES branch (addr)
 );
-
+    
 CREATE TABLE loan (
     amount          NUMERIC(10,2),
     runtime         INTEGER,
     schedule        INTEGER,
-    rate            NUMERIC(2,5)
+    rate            NUMERIC(4,2)
 );
 
-CREATE DOMAIN trans_t AS
-    VARCHAR NOT NULL CHECK (value = "WITHDRAWAL" OR value = "DEPOSIT");
+CREATE TYPE trans_t AS ENUM ('DEPOSIT', 'WITHDRAWAL');
 
 CREATE TABLE transactions (
     trans_type      trans_t,
     amount          NUMERIC(10,2) NOT NULL,
     description     VARCHAR(200)
-)
+);
 
