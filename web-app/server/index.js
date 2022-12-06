a2458bbe873d2d9b31b09333c3f3ea9d5b92f9a1
Charles Saineghi <charles@delltoo.localdomain>
2022-12-06 13:15:07 -0600

    use postgres

--- web-app/server/index.js
+++ web-app/server/index.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const mysql = require('mysql');
+const { db } = require('./connect');
 const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-const db = mysql.createConnection({
-  user: 'postgres',
-  host: 'localhost',
-  password: 'admin',
-  database: 'postgres',
-});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Postgres connection error', err.stack)
+  } else {
+    console.log('connected to Postgres')
+  }
+})
 
 app.post('/register', (req,res) => {
+    console.log("bitchass")
     const fullname = req.body.fullname
     const password = req.body.password
     const mailingaddress = req.body.mailingaddress
-    const branch = req.body.branch
-    db.query("INSERT INTO users (fullname, password) VALUES (?,?)"
+    const branch = req.body.branch  // branch is to be entered as an ID
+    db.query("INSERT INTO customer(name, passwd, addr, branch) VALUES($1,$2,$3,$4);",
     [fullname,password,mailingaddress,branch],
     (err,result) => {
         console.log(err);
