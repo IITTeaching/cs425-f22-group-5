f316de2451185e3a72bba183b01c57a97a46185a
Charles Saineghi <charles@delltoo.localdomain>
2022-12-06 19:07:17 -0600

    update types and change primary keys

--- init_db.sql
+++ init_db.sql
@@ -13,20 +13,24 @@ CREATE TABLE branch (
     PRIMARY KEY     (branch_id)
 );
 
+CREATE TYPE emp_t AS ENUM ('TELLER', 'MANAGER');
+
 CREATE TABLE employee (
-    ssn             INTEGER NOT NULL,
+    ssn             NUMERIC(9,0),
     name            VARCHAR(50),
+    username        VARCHAR(50),
     passwd          VARCHAR(50),
     addr            VARCHAR(20),
     branch          INTEGER,         -- foreign key to branch_id in branch
-    position        VARCHAR(20),
+    position        emp_t,
     salary          NUMERIC(10,2),
     PRIMARY KEY     (ssn),
-    FOREIGN KEY     (branch) REFERENCES branch (addr)
+    FOREIGN KEY     (branch) REFERENCES branch (branch_id)
 );
 
 CREATE TABLE customer (
     customer_id     SERIAL,
+    username        VARCHAR(50),
     name            VARCHAR(50),
     passwd          VARCHAR(50),
     addr            VARCHAR(20),
@@ -39,17 +43,19 @@ CREATE TYPE account_t AS ENUM ('SAVINGS', 'CHECKING');
 
 CREATE TABLE account (
     account_owner   INTEGER,
-    account_num     INTEGER NOT NULL,
+    account_num     SERIAL,
     account_type    account_t,
     balance         NUMERIC(10,2),
+    PRIMARY KEY     (account_owner, account_num),
     FOREIGN KEY     (account_owner) REFERENCES customer
 );
 
 CREATE TABLE account_type (
     account_type    account_t,
     fee             NUMERIC(4,2),
-    addr            VARCHAR(20),     -- foreign key to addr in branch
-    FOREIGN KEY     (addr) REFERENCES branch (addr)
+    branch          INTEGER,
+    --addr            VARCHAR(20),     -- foreign key to addr in branch
+    FOREIGN KEY     (branch) REFERENCES branch (branch_id)
 );
     
 CREATE TABLE loan (
