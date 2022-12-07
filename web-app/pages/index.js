9d1abb10f04b77cc230853104945d7c69b2b22df
Charles Saineghi <charles@delltoo.localdomain>
2022-12-06 19:09:25 -0600

    change `class` to `className`

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -21,20 +21,20 @@ export default function Home() {
      <div className="text-center  ">
       
         
-        <section class="py-24 flex items-center min-h-screen justify-center bg-white">
-  <div class="mx-auto max-w-[43rem]">
-    <div class="text-center">
-      <h1 class="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black">What type of user are you?</h1>
+        <section className="py-24 flex items-center min-h-screen justify-center bg-white">
+  <div className="mx-auto max-w-[43rem]">
+    <div className="text-center">
+      <h1 className="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black">What type of user are you?</h1>
     </div>
 
-    <div class="mt-6 flex items-center justify-center gap-4">
+    <div className="mt-6 flex items-center justify-center gap-4">
       <Link href = "" onClick={handleClick}>
-      <button href="#" class="transform rounded-md bg-indigo-600/95 px-5 py-3 font-medium text-white transition-colors hover:bg-indigo-700">Customer</button>
+      <button href="#" className="transform rounded-md bg-indigo-600/95 px-5 py-3 font-medium text-white transition-colors hover:bg-indigo-700">Customer</button>
 
       </Link>
      
       <Link href = "" >
-      <button href="#" class="transform rounded-md border border-slate-200 px-5 py-3 font-medium text-slate-900 transition-colors hover:bg-slate-50"> Employee </button>
+      <button href="#" className="transform rounded-md border border-slate-200 px-5 py-3 font-medium text-slate-900 transition-colors hover:bg-slate-50"> Employee </button>
       </Link>
 
      
