43062585031a4de84c5de93e16d9fff0102d108c
Charles Saineghi <charles@delltoo.localdomain>
2022-12-08 15:26:23 -0600

    if statements in prompt now compares to strings and not integers

--- main.py
+++ main.py
@@ -28,14 +28,15 @@ def emp_login():
     pass # TODO basically same as cust_login
 
 def login(): # will return user_id of the person logged in
+    print("")
     while(True):
         print("Are you logging in as a...")
         print("1.) Customer")
         print("2.) Employee")
         usertype = input(">>> ")
-        if usertype == 1:
+        if usertype == '1':
             return ('customer', cust_login())
-        elif usertype == 2:
+        elif usertype == '2':
             return ('employee', emp_login())
         else:
             print("Invalid input.")
@@ -59,6 +60,21 @@ def register():
     c.execute("INSERT INTO customer (username, name, passwd, addr, branch) values (%s, %s, %s, %s, %s)",
      (uname, fname, passwd, addr, branch))
     c.close()
+
+def create_account(user_id):
+    c = conn.cursor()
+    while(True):
+        print("Select from the following")
+        print("1.) Create new checking account")
+        print("2.) Create new savings account")
+        print("Press anything else to abort.")
+        t = input(">>> ")
+        if t == '1':
+            pass
+        elif t == '2':
+            pass
+        else:
+            return
 
 def deposit(acc_num, user_id):
     pass # TODO: Validate account number with user; Prompt for amount and adjust balance
@@ -87,9 +103,9 @@ def main():
     while not loggedInAs:
         print("Enter 1 to login, or 2 to register a new user account.")
         i = input(">>> ")
-        if i == 1:
+        if i == '1':
             loggedInAs = login()
-        elif i == 2:
+        elif i == '2':
             register()
     
     if loggedInAs[0] == 'customer':
@@ -101,19 +117,37 @@ def main():
             print("4.) Create a new account")
             print("Enter anything else to exit")
             i = input(">>> ")
-            if i == 1:
+            if i == '1':
                 pass # TODO do a query and make it pretty
-            elif i == 2:
+            elif i == '2':
                 pass # call deposit()
-            elif i == 3:
+            elif i == '3':
                 pass # call withdrawal()
-            elif i == 4:
+            elif i == '4':
                 pass # uhhh
             else:
                 break
 
     elif loggedInAs[0] == 'employee':
-        pass
+        while(True):
+            print("Please select from the following: ")
+            print("1.) View account info")
+            print("2.) Deposit into an account")
+            print("3.) Withdraw from an account")
+            print("4.) Create a new account")
+            print("Enter anything else to exit")
+            i = input(">>> ")
+            if i == '1':
+                pass # TODO do a query and make it pretty
+            elif i == '2':
+                pass # call deposit()
+            elif i == '3':
+                pass # call withdrawal()
+            elif i == '4':
+                pass # uhhh
+            else:
+                break
+            
 
     conn.close()
     
